c7d138fb0aa99c7decd6ea173754ebd34bcdf7b8
Osmar Junior <oj13black@hotmail.com>
2021-05-05 08:57:18 -0300

    web scraping finalizado

--- OneBitCode/web_scrap.rb
+++ OneBitCode/web_scrap.rb
@@ -13,12 +13,24 @@ require 'net/http'
 # h1 = doc.at('h1')
 # puts h1.content
 
+# https = Net::HTTP.new('onebitcode.com', 443)
+# https.use_ssl = true;
+# response = https.get("/")
+
+# doc = Nokogiri::HTML(response.body)
+# last_post = doc.at('h3 a')
+
+# puts last_post.content
+# puts last_post['href']
+
 https = Net::HTTP.new('onebitcode.com', 443)
-https.use_ssl = true;
+https.use_ssl = true
+
 response = https.get("/")
 
 doc = Nokogiri::HTML(response.body)
-last_post = doc.at('h3 a')
-
-puts last_post.content
-puts last_post['href']
+doc.search('h3 a').each do |a|
+    puts a.content
+    puts a['href']
+    puts '---'
+end
